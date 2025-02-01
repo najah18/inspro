3986a62becf2b162a3d55a76da4ae3ecd20b4bf3
najah18 <nanaalsaker123@gmail.com>
2025-02-01 08:35:36 +0200

    archiv

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta name="google-site-verification" content="3izOAw8skXi7miijVXtcjTwqvTmNoyjutk8UqXWXzWY" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
