09655401173fdcb2d9cf4446e878a00401ce9b0e
najah18 <nanaalsaker123@gmail.com>
2025-02-11 21:20:45 +0200

    fix

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Income extends Model
+class Income extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = ['name', 'description', 'price', 'date'];
 
-   
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('incomes')
+             ->singleFile(); // إذا كانت الصورة واحدة فقط لكل دخل
+    }
 }
